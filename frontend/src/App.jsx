78827c33970117e71834b8fa622493ecee6e980b
React.js <aditya@192.168.1.6>
2024-09-19 22:47:46 +0530

    adding changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,21 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import Header from "./components/Header";
-import PostForm from "./Requests/PostForm";
+import PostForm from "./Operations/PostForm";
+
 function App() {
   const [data, setData] = useState([{}]);
 
   useEffect(() => {
+    getUsers();
+  }, []);
+  function getUsers() {
     fetch("http://localhost:3000/api/books")
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  }
+
   const handleDelete = (id) => {
     fetch(`http://localhost:3000/api/books/${id}`, {
       method: "DELETE",
@@ -20,6 +25,7 @@ function App() {
         if (response.ok) {
           setData(data.filter((book) => book.id !== id));
         }
+        getUsers();
       })
       .catch((error) => console.error("Error deleting data:", error));
   };
@@ -30,10 +36,9 @@ function App() {
       <div className="min-h-scree flex">
         <div className="ml-1/3 w-2/3 p-6 overflow-auto">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-            {data.map((book) => (
-              <div>
+            {data.map((book, index) => (
+              <div key={index}>
                 <Card
-                  key={book.id}
                   title={book.title}
                   author={book.author}
                   publishedDate={book.publishedDate}
@@ -41,21 +46,17 @@ function App() {
                   isbn={book.isbn}
                 />
                 <button
-                  onClick={() => handleDelete(book.id)}
+                  onClick={() => handleDelete(book._id)}
                   className=" bg-indigo-500 text-white mt-3 py-2 px-4 mr-3 rounded ">
                   {" "}
                   Delete
-                </button>
-                <button className=" bg-indigo-500 text-white mt-3   py-2 px-4 rounded">
-                  {" "}
-                  Edit
                 </button>
               </div>
             ))}
           </div>
         </div>
         <div>
-          <PostForm />
+          <PostForm onBookAdded={getUsers} />
         </div>
       </div>
     </>
