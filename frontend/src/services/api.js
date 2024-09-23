a31a4e51f71e367e1a6c17fd670507ef06fc1510
React.js <aditya@192.168.1.4>
2024-09-24 00:40:54 +0530

    merging changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 export async function fetchData(url, method = "GET", body = {}) {
-  return fetch(`http://ec2-18-222-170-221.us-east-2.compute.amazonaws.com${url}`, {
+  return fetch(`http://localhost:3000${url}`, {
     method: method,
     headers: {
       "Content-Type": "application/json",
@@ -11,7 +11,7 @@ export async function fetchData(url, method = "GET", body = {}) {
 export const deleteBook = async (id) => fetchData(`/api/books/${id}`, "DELETE");
 export const PostUpdateBook = async (activeBookId, bookData) => {
   const response = await fetch(
-    `hhttp://ec2-18-222-170-221.us-east-2.compute.amazonaws.com/api/books/${activeBookId}`,
+    `http://localhost:3000/api/books/${activeBookId}`,
     {
       method: `${activeBookId ? "PUT" : "POST"}`,
       headers: {
